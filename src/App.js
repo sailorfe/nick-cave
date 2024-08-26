0ba4cef1d8ebce218b321f0c8c6ebcb80073e1b6
kdlr <sailorfe@proton.me>
2024-08-26 12:44:32 -0400

    big progress

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import './index.css';
 import JSONDATA from '../src/nick-cave-lyrics/lyrics.json';
@@ -10,16 +9,23 @@ function App() {
 
     return (
         <div className="App">
-            <input type="text" placeholder="seach..." onChange={e=>setSearchTerm(e.target.value)} />
+            <input type="text" placeholder="search lyrics..." onChange={e=>setSearchTerm(e.target.value)} />
             {JSONDATA.filter((val)=>{
                 if(searchTerm == ""){
-                    return val
+                    return 
                 }
                 else if(val.lyric.toLowerCase().includes(searchTerm.toLowerCase())){
                     return val;
                 }
             }).map((val,key)=>{
-                return <div>{val.lyric} </div>
+                return <article>
+                        <h2>{val.song} – <em>{val.album}</em></h2>
+                        <ul>
+                            <li>{val.prev}</li>
+                            <li>{val.lyric}</li>
+                            <li>{val.next}</li>
+                        </ul>
+                    </article>
             })
             }
         </div>
